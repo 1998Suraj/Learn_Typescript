214c254b6c02fe95d1a39fe2af83b08bae68e3de
Suraj Karosia <surajkarosia98@gmail.com>
2024-04-25 18:12:06 +0530

    Declare interface

--- day_1_learn_basic.ts
+++ day_1_learn_basic.ts
@@ -1,2 +1,15 @@
 const firstName: string = "suraj";
 console.log("hello" + " " + firstName);
+
+interface User {
+    name: string;
+    age: number;
+    email: string;
+}
+
+const user: User = {
+    name: "suraj",
+    age:18,
+    email:"surajkarosia98@gmail.com"
+}
+console.log(user);
