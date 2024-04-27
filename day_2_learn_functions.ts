2fda4aace85988ded88ff461ff81ae3eb1c32ffe
Suraj Karosia <surajkarosia98@gmail.com>
2024-04-27 20:01:15 +0530

    uploads

--- day_2_learn_functions.ts
+++ day_2_learn_functions.ts
@@ -86,4 +86,32 @@ let pickedCard1 = cardPicker1();
 
 console.log("pickedCard1: ", pickedCard1);
 
+let suits = ["hearts", "spades", "clubs", "diamonds"];
+ 
+function pickCard(x: any): any {
+  // Check to see if we're working with an object/array
+  // if so, they gave us the deck and we'll pick the card
+  if (typeof x == "object") {
+    let pickedCard = Math.floor(Math.random() * x.length);
+    return pickedCard;
+  }
+  // Otherwise just let them pick the card
+  else if (typeof x == "number") {
+    let pickedSuit = Math.floor(x / 13);
+    return { suit: suits[pickedSuit], card: x % 13 };
+  }
+}
+ 
+let myDeck = [
+  { suit: "diamonds", card: 2 },
+  { suit: "spades", card: 10 },
+  { suit: "hearts", card: 4 },
+];
+ 
+let pickedCard3 = myDeck[pickCard(myDeck)];
+console.log("pickedCard3: ", pickedCard3);
+ 
+let pickedCard4 = pickCard(15);
+console.log("pickedCard4: ", pickedCard4);
+
 
