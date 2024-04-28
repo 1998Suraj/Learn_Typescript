c1dfed4add6b1a6939caa905df7bb01356f504a8
Suraj Karosia <surajkarosia98@gmail.com>
2024-04-28 21:09:13 +0530

    gyfgsyrtj

--- day_2_learn_functions.ts
+++ day_2_learn_functions.ts
@@ -151,3 +151,16 @@ const n = firstElement([1, 2, 3]);
 // u is of type undefined
 const u = firstElement([]);
 console.log("s: ", s, "n: ", n, "u: ", u);
+
+function firstElement1<Type>(arr: Type[]) {
+    return arr[0];
+  }
+   
+  function firstElement2<Type extends any[]>(arr: Type) {
+    return arr[0];
+  }
+   
+  // a: number (good)
+  const a = firstElement1([1, 2, 3]);
+  // b: any (bad)
+  const b = firstElement2([1, 2, 3]);
